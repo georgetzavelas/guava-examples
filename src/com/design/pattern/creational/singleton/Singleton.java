318ea06d55b069a2897b1e73855a3b53bd98c143
George Tzavleas <george.tzavelas@gmail.com>
2016-03-09 10:57:46 -0500

    updated comments for singleton

--- src/com/design/pattern/creational/singleton/Singleton.java
+++ src/com/design/pattern/creational/singleton/Singleton.java
@@ -8,6 +8,11 @@ public class Singleton {
     /**
      * SingletonHolder is loaded on the first execution of Singleton.getInstance()
      * or the first access to SingletonHolder.INSTANCE, not before.
+     *
+     * https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom#Example_Java_Implementation
+     *
+     * Despite the elegance of this approach any failure to initialize renders the class unusable
+     * which means the holder pattern can only be used when the programmer is certain the initialization will not fail.
      */
     private static class SingletonHolder {
         private static final Singleton INSTANCE = new Singleton();
